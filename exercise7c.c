3b92cf976f69873688e6a13c6318656ba71c3e0b
peltonenmikko <mikkopee9@gmail.com>
2023-03-21 22:52:34 +0200

    7c complete

--- exercise7c.c
+++ exercise7c.c
@@ -3,22 +3,49 @@
 #include <string.h>
 #include <time.h>
 
+int guess_the_number(int number_to_be_guessed, int *tries_used);
 int main(void)
 {
     srand(time(NULL));
-    int guessed_num = rand() % 100 + 1;
+    int number_to_be_guessed = rand() % 100 + 1;
     int tries_used;
-    int result = guess_the_number(guessed_num, &tries_used);
-    
+    int result = guess_the_number(number_to_be_guessed, &tries_used);
     if (result == 1)
     {
     printf("You won! It took you %d tries.", tries_used);
     }
-
     else
     {
-    printf("You lost! The number was %d.", guessed_num);
+    printf("You lost! The number was %d.", number_to_be_guessed);
     }
-
     return 0;
+}
+
+int guess_the_number(int number_to_be_guessed, int *tries_used)
+{
+    int number_guessed = 0, b = 0;
+    printf("Guess the number between 0 and 100: ");
+    for(b=0; b<5; b++)
+    {
+        scanf("%d", &number_guessed);
+        if(number_guessed == number_to_be_guessed)
+        {
+            *tries_used = b+1;
+            return 1;
+        }
+        else
+        {
+            if(b<4)
+            {
+                if(number_guessed > number_to_be_guessed)
+                {
+                    printf("Wrong number. Correct number is smaller: ");
+                }
+                else
+                {
+                    printf("Wrong number. Correct number is bigger: ");
+                }
+            }
+        }
+    }
 }
